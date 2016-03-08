074d73e416127304bf8830299ec0841fd57948e8
Liu Lantao <liulantao@gmail.com>
2016-03-08 13:12:32 +0800

    rackup

---
+++ config.ru
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby -w
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
+require './readable.rb'
+
+run ReadableApp
--- readable.rb
+++ readable.rb
@@ -7,11 +7,25 @@ Bundler.require
 
 require 'open-uri'
 
-get '/api/readable' do
-	url = params[:url] || 'https://Readom.github.io/readable/'
-	source = open(url).read
-	options = {:tags => %w[div p img pre code a], :attributes => %w[src href], :remove_empty_nodes => false, :ignore_image_format => []}
-	result = Readability::Document.new(source, options).content
+class ReadableApp < Sinatra::Base
 
-	result
+  enable :sessions
+
+  configure {
+    set :server, :puma
+  }
+
+  helpers do
+    include Rack::Utils
+    alias_method :h, :escape_html
+  end
+
+  get '/api/readable' do
+    url = params[:url] || 'https://Readom.github.io/readable/'
+    source = open(url).read
+    options = {:tags => %w[div p img pre code a], :attributes => %w[src href], :remove_empty_nodes => false, :ignore_image_format => []}
+    result = Readability::Document.new(source, options).content
+
+    result
+  end
 end
